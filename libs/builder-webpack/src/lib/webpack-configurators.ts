0114939c310e0fb8d4823a8da59858f7ea908baf
gund <malkevich.alex@gmail.com>
2020-01-17 18:26:34 +0100

    perf(builder-webpack): cache config merge strategies in configurators

--- libs/builder-webpack/src/lib/webpack-configurators.ts
+++ libs/builder-webpack/src/lib/webpack-configurators.ts
@@ -3,25 +3,24 @@ import * as webpackMerge from 'webpack-merge';
 
 import { WebpackConfigurator } from './configurator';
 
+const APPEND_RULES = webpackMerge.strategy({ 'module.rules': 'append' });
+const APPEND_PLUGINS = webpackMerge.strategy({ plugins: 'append' });
+const APPEND_ENTRY = webpackMerge.strategy({ entry: 'append' });
+
 export function webpackAppendRules(
   rules: webpack.RuleSetRule[]
 ): WebpackConfigurator {
-  return config =>
-    webpackMerge.strategy({ 'module.rules': 'append' })(config, {
-      module: { rules }
-    });
+  return config => APPEND_RULES(config, { module: { rules } });
 }
 
 export function webpackAppendPlugins(
   plugins: webpack.Plugin[]
 ): WebpackConfigurator {
-  return config =>
-    webpackMerge.strategy({ plugins: 'append' })(config, { plugins });
+  return config => APPEND_PLUGINS(config, { plugins });
 }
 
 export function webpackAppendEntry(
   entry?: string | string[] | webpack.Entry | webpack.EntryFunc
 ): WebpackConfigurator {
-  return config =>
-    webpackMerge.strategy({ plugins: 'append' })(config, { entry });
+  return config => APPEND_ENTRY(config, { entry });
 }
